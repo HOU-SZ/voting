00ee8f3c52d91402cc47f0ea1cf1caead0931ad0
Shizheng Hou [I] <shihou@tesla.com>
2021-10-06 00:53:29 +0800

    Add basic api

---
+++ client/src/api/index.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const BASE_URL = "https://localhost:5000/api/"; //Backend api
+
+export const ENDPOINTS = {
+    TOPICS: "topics",
+    LOGIN: "users/login",
+    REGISTER: "users/register",
+    LOGOUT: "users/logout"
+}
+
+axios.interceptors.request.use(config => {
+    let token = localStorage.getItem("jwtToken");
+    if (token) { 
+        config.headers.Authorization = 'Bearer ' + token;
+    }
+    return config
+}, error => {
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(res => {
+    return res
+}, error => {
+    if (error.response.status === 401) {
+    }
+    return Promise.reject(error)
+})
+
+export const createAPIEndpoint = endpoint => {
+
+    const url = BASE_URL + endpoint;
+        return {
+            fetchAll: () => axios.get(url),
+            fetchById: id => axios.get(url + '/' + id),
+            create: newRecord => axios.post(url, newRecord),
+            update: (updateRecord) => axios.put(url, updateRecord),
+            // update: (id, updateRecord) => axios.put(url + id, updateRecord),
+            delete: id => axios.delete(url + '/' + id),
+        }
+}
